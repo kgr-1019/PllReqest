0c4833bcb9f27c89d5e507ec528083ab6d14e9f5
k023g0009@g.neec.ac.jp <k023g0009@g.neec.ac.jp>
2023-10-30 10:42:37 +0900

    PullRequest

--- Program.vb
+++ Program.vb
@@ -3,5 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+
+        Console.WriteLine("PullRequest branch changed")
     End Sub
 End Module
